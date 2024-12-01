890a6bbd335417e34753624e08ccbeec9c8960e3
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-02 00:02:15 +0530

    Update lineage_lavender.mk

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -14,7 +14,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-WITH_GMS := true
+# WITH_GMS := true
 PRODUCT_NAME := lineage_lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
