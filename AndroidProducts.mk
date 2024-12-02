e629db672836d16ae6a6d404ad6a9bbabc653eaa
miteshx07 <130374982+miteshx07@users.noreply.github.com>
2024-12-02 22:23:02 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    lineage_lavender-user
